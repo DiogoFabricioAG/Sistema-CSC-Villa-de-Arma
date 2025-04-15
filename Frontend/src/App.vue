10e054b6a5df4c8268040ec869e8a51683a55588
DiogoFabricioAG <diogo.abregu.g@uni.pe>
2025-04-15 00:08:20 -0500

    chore: animiaciones en el cambio de view

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -5,6 +5,20 @@ import Header from "./components/Header.vue";
 <template>
   <Header />
   <div class="w-[90vw] md:w-[80vw] my-2 mx-auto">
-    <router-view />
+    <router-view v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <Component :is="Component" />
+      </transition>
+    </router-view>
   </div>
 </template>
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- Frontend/src/pages/Home.vue
+++ Frontend/src/pages/Home.vue
@@ -4,27 +4,29 @@ import BasicContainer from "../components/BasicContainer.vue";
 import MayorContainer from "../components/MayorContainer.vue";
 </script>
 <template>
-  <h1 class="font-primary text-center font-bold text-3xl">Bienvenido/a 👋</h1>
+  <div>
+    <h1 class="font-primary text-center font-bold text-3xl">Bienvenido/a 👋</h1>
 
-  <MayorContainer>
-    <BasicContainer container-type="1third">
-      <p class="font-primary text-2xl font-semibold">Acciones del Sistema</p>
-      <div class="flex flex-col gap-2 w-full items-center">
-        <RouterIcon to="census" icon="user" text="Padrón de Socios" />
-        <RouterIcon to="assembly" icon="assembly" text="Asamblea de Socios" />
-        <RouterIcon to="debt" icon="tag" text="Deudas de Socios" />
-      </div>
+    <MayorContainer>
+      <BasicContainer container-type="1third">
+        <p class="font-primary text-2xl font-semibold">Acciones del Sistema</p>
+        <div class="flex flex-col gap-2 w-full items-center">
+          <RouterIcon to="census" icon="user" text="Padrón de Socios" />
+          <RouterIcon to="assembly" icon="assembly" text="Asamblea de Socios" />
+          <RouterIcon to="debt" icon="tag" text="Deudas de Socios" />
+        </div>
+      </BasicContainer>
+
+      <BasicContainer container-type="2thirds">
+        <p class="font-primary text-2xl font-semibold">Deudas Totales</p>
+        <div class="flex flex-col gap-2 w-full items-center">
+          <p>dasdasml</p>
+        </div>
+      </BasicContainer>
+    </MayorContainer>
+
+    <BasicContainer>
+      <p class="font-primary text-2xl font-semibold">Notificaciones 🔔</p>
     </BasicContainer>
-
-    <BasicContainer container-type="2thirds">
-      <p class="font-primary text-2xl font-semibold">Deudas Totales</p>
-      <div class="flex flex-col gap-2 w-full items-center">
-        <p>dasdasml</p>
-      </div>
-    </BasicContainer>
-  </MayorContainer>
-
-  <BasicContainer>
-    <p class="font-primary text-2xl font-semibold">Notificaciones 🔔</p>
-  </BasicContainer>
+  </div>
 </template>
